e233ac4854a74f99e6f7576da51a263b7f0310a8
ebalo <ebalo55@users.noreply.github.com>
2024-02-24 10:41:21 +0100

    chore: test with coverage report, plugin system facade creation
    Signed-off-by: ebalo <ebalo55@users.noreply.github.com>

--- libs/plugin-system/src/server.ts
+++ libs/plugin-system/src/server.ts
@@ -1,2 +1,4 @@
 // Use this file to export React server components
-export * from "./lib/hello-server";
+
+export * from "./lib/interfaces/plugin-definition.interface";
+export * from "./lib/plugin-system";
