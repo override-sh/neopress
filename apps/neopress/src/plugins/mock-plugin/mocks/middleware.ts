7f6be338b42585d6995dc923b00c2b31976c4e7b
ebalo <12586380+ebalo55@users.noreply.github.com>
2024-02-28 08:30:14 +0100

    test: added mock component for each of the available positions, added a test

--- apps/neopress/src/plugins/mock-plugin/mocks/middleware.ts
+++ apps/neopress/src/plugins/mock-plugin/mocks/middleware.ts
@@ -4,6 +4,10 @@ import {
 } from "next/server";
 
 export const MockMiddleware = async (req: NextRequest) => {
+    if(process.env.SKIP_MIDDLEWARE) {
+        return NextResponse.next();
+    }
+
     return NextResponse.json({
         message: "Hello, World!",
     });
